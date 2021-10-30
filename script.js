5cca3332c8c7b0f5ca05366eb78ab5c667e7f7f9
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-10-30 13:58:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// let a = prompt("Введите а");
+// let b = prompt("Введите b");
+// let c = prompt("Введите c");
+let functions = [
+    [-4, 4, 1],
+    [-6, 6, 1],
+    [-8, 8, 1],
+    [-5, 5, 1],
+    [-7, 7, 1],
+    [-9, 9, 1],
+    [-10, 10, 1]
+];
+function printTable(from, to, step, func) {
+    if (func === undefined) {
+        return;
+    }
+    from = from;
+    to = to;
+    step = step || 1;
+    document.write("<table border='1' cellspacing='0'>");
+    for (var x = from; x <= to; x += step) {
+        document.write("<tr>");
+        document.write("<td>" + x + "</td>");
+        document.write("<td>" + func(x) + "</td>");
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
+
+function quadratic(x) {
+    return x * x;
+}
+
+
+let rand = Math.floor(Math.random() * functions.length);
+printTable(functions[rand][0], functions[rand][1], functions[rand][2], quadratic);
+// printTable(a = prompt("Введите а"), b = prompt("Введите b"), c = prompt("Введите c"), quadratic);
+
+// printTable(-4, 4, 1, quadratic);
+
+// printTable(-4, 4, 0.1, Math.sin);
+
+// printTable(0, 10, 2, function (x) { return x / 2; });
